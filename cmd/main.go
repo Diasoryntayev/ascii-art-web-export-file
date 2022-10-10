3495b12ddd77b48688c36b5e26e276a4bd64d687
Dias12 <diasoryntayev@gmail.com>
2022-10-10 13:17:27 +0600

    update func main

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.Handle("/ui/static/", http.StripPrefix("/ui/static/", http.FileServer(http.Dir("./ui/static"))))
 
 	log.Println("Запуск веб-сервера на http://127.0.0.1:8080")
 	err := http.ListenAndServe(":8080", mux)
