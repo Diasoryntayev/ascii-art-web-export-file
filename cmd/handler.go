7df740716fe705a18d16b3cf0aa1bda445a8c993
Dias12 <diasoryntayev@gmail.com>
2022-10-10 13:18:10 +0600

    update func home

--- cmd/handler.go
+++ cmd/handler.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello world!"))
+	if r.URL.Path != "/" {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+	if r.Method != http.MethodGet {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+	ts, err := template.ParseFiles("../ui/html/index.html")
+	if err != nil {
+		fmt.Println("1: ", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	err = ts.Execute(w, nil)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		fmt.Println("2: ", err)
+		return
+	}
 }
