62c980cbb1c1bc4de5e1ba17cd1ba4f6c6c6d821
Dias12 <diasoryntayev@gmail.com>
2022-10-12 12:45:40 +0600

    feat: add handleFunc ascii

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/ascii-art", ascii)
 	mux.Handle("/ui/static/", http.StripPrefix("/ui/static/", http.FileServer(http.Dir("./ui/static"))))
 
 	log.Println("Запуск веб-сервера на http://127.0.0.1:8080")
